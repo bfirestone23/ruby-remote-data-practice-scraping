fc7d107ad6149aed834e5e2201fb511b9e5163b1
Brian Firestone <bfirestone2339@gmail.com>
2020-12-28 15:23:20 -0500

    commit tree 326874b822adce32d7c5a87fbde4699529feedfc

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+p doc.css(".imageTextBlockGrid2-3jXtmC.inlineMobileLeft-2Yo002")[0].children
